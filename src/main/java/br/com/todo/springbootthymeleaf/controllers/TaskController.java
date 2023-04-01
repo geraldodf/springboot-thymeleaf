34ad8ecf2bfee8ca9bf9242b45110a9bc4b186cf
geraldodf <geraldodarosfilho@hotmail.com>
2023-04-01 19:02:36 +0200

    Create post method and fixinh bugs

--- src/main/java/br/com/todo/springbootthymeleaf/controllers/TaskController.java
+++ src/main/java/br/com/todo/springbootthymeleaf/controllers/TaskController.java
@@ -1,12 +1,17 @@
 package br.com.todo.springbootthymeleaf.controllers;
 
-import br.com.todo.springbootthymeleaf.models.entities.Task;
-import br.com.todo.springbootthymeleaf.models.repositories.TaskRepository;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.todo.springbootthymeleaf.dtos.TaskDto;
+import br.com.todo.springbootthymeleaf.models.entities.Task;
+import br.com.todo.springbootthymeleaf.models.repositories.TaskRepository;
 
 @Controller
 public class TaskController {
@@ -14,11 +19,17 @@ public class TaskController {
     @Autowired
     private TaskRepository repo;
 
-    @GetMapping("/home")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
-        Task task = new Task("Estudar", "Resolver bug do Thymeleaf");
-        repo.save(task);
+    @GetMapping("/tasks")
+    public String getAllTasks(Model model) {
+        ArrayList<Task> tasks = (ArrayList<Task>) repo.findAll();
+        model.addAttribute("tasks", tasks);
         return "home";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask(TaskDto dto) {
+        Task task = new Task(dto.getName(), dto.getDescription());
+        repo.save(task);
+        return "redirect:/tasks";
     }
 }
---
+++ src/main/java/br/com/todo/springbootthymeleaf/dtos/TaskDto.java
@@ -0,0 +1,14 @@
+package br.com.todo.springbootthymeleaf.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDto {
+
+    private String name;
+    private String description;
+    
+    
+}
